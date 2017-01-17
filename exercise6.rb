b9776c8527f0586ca451f18c6cf118b1bc7dfe09
Carey Costa <careyxcosta@gmail.com>
2017-01-17 14:42:04 -0500

    exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,7 @@
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+grocery_list = ["* carrots", "* toilet paper", "* apples", "* salmon"]
+grocery_list.push("* rice")
+grocery_list.length
+grocery_list.include?("bananas")
+grocery_list[1]
+grocery_list.sort
+grocery_list.pop(3)
