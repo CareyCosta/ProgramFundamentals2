3c455d21c8884cdbffdc01dcc70f752d4e9930ca
Carey Costa <careyxcosta@gmail.com>
2017-01-17 16:18:24 -0500

    exercise 5 complete

--- exercise5.rb
+++ exercise5.rb
@@ -1,3 +1,10 @@
-def tempconvert(num)
-  (num - 32) * 0.5555
+puts "What is the temperature in fahrenheit?"
+answer = gets.chomp.to_i
+
+def tempconvert(answer)
+  (answer - 32) * 0.5555
 end
+
+c = tempconvert(answer)
+
+puts "The temperature in celsius is #{c}"
